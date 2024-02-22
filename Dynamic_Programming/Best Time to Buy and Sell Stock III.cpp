7ddc346d66c9d789eb1b37a7b50e63e86c004298
Nikhil Thakur <90825439+nikhil-thakurr@users.noreply.github.com>
2024-02-22 08:38:34 +0530

    Create  Best Time to Buy and Sell Stock III.cpp

---
+++ Dynamic_Programming/Best Time to Buy and Sell Stock III.cpp
@@ -0,0 +1,22 @@
+Q-Link : https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/description/
+
+class Solution {
+public:
+ int helper(vector<int>&prices,int idx,int n,int buy,int cap,         vector<vector<vector<int>>>&dp){
+        if(n==idx||cap==0)return 0;
+        if(dp[idx][buy][cap]!=-1)return dp[idx][buy][cap];
+        int profit=0;
+        if(buy)profit=max(-1*prices[idx]+helper(prices,idx+1,n,0,cap,dp),helper(prices,idx+1,
+        n,1,cap,dp));
+        else profit=max(prices[idx]+helper(prices,idx+1,n,1,cap-1,dp),helper(prices,idx+1,n,0,cap,dp));
+
+        return dp[idx][buy][cap]=profit;
+
+    }
+
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
+        return helper(prices,0,n,1,2,dp);
+    }
+};
