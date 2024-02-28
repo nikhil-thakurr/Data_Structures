b64fe58904bbfdab39e0bdec4ff9b4c71e6cf99d
Nikhil Thakur <90825439+nikhil-thakurr@users.noreply.github.com>
2024-02-28 09:15:11 +0530

    Update Longest Increasing Subsequence.cpp

--- Dynamic_Programming/Longest Increasing Subsequence.cpp
+++ Dynamic_Programming/Longest Increasing Subsequence.cpp
@@ -157,3 +157,35 @@ public:
     }
 };
 
+Approach 7 : Printing LIS
+
+     vector<int>dp(n,1),hash(n);
+        int maxi=-1;
+        int last_Idx=0;
+
+        for(int i=0;i<n;i++){
+            hash[i]=i;
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j] && dp[i]<1+dp[j]){
+                    dp[i]=1+dp[j];
+                    hash[i]=j;
+                }
+                
+            }
+            if(dp[i]>maxi){
+                maxi=dp[i];
+                last_Idx=i;
+            }
+        }
+        
+        vector<int>ans;
+        
+        while(hash[last_Idx]!=last_Idx){
+            ans.push_back(nums[last_Idx]);
+            last_Idx=hash[last_Idx];
+        }
+        ans.push_back(nums[last_Idx]);
+        reverse(ans.begin(),ans.end());
+        
+        return ans;
+
